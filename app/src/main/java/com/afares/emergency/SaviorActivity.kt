8ed853710570128676daa5ccc2754a68b9219656
AFares1 <ahmedfaresa10@gmail.com>
2021-02-22 00:51:21 +0200

    Savior Activity Design adding nav_savior, recyclerView and request details layout

--- app/src/main/java/com/afares/emergency/SaviorActivity.kt
+++ app/src/main/java/com/afares/emergency/SaviorActivity.kt
@@ -2,10 +2,21 @@ package com.afares.emergency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class SaviorActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_savior)
+
+        navController = findNavController(R.id.navHostFragment)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
